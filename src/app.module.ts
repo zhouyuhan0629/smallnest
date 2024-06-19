c0bf74bb40a2dfc0369ae580270dcf07836c20a6
yuhan zhou <zhouyuhan@pingansec.com>
2024-06-19 18:29:34 +0800

    ff

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config'
-// 应用程序的根模块。
+import { CatsController } from './cats/cats.controller';
+// 应用程序的根模块。模块类不能注入到提供者中。
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
+  controllers: [AppController, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
