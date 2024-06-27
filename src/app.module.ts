7e0e32abb8a4e01bf1f7496d8fa509c080aa8767
Your Name <you@example.com>
2024-06-27 18:40:46 +0800

    11

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CatsController } from './cats/cats.controller';
 // 应用程序的根模块。模块类不能注入到提供者中。
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+     ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: '.env',
+  }),
+  TypeOrmModule.forRootAsync({
+    inject: [ConfigService], // 注入 ConfigService 以便获取配置信息
+    useFactory: (configService: ConfigService) => ({
+      type: 'mysql',
+      host: configService.get<string>('DB_HOST'),
+      port: configService.get<number>('DB_PORT'),
+      username: configService.get<string>('DB_USERNAME'),
+      password: configService.get<string>('DB_PASSWORD'),
+      database: configService.get<string>('DB_NAME'),
+      entities: [], // 需要加载的实体类
+      synchronize: true, // 设置为 true 时会自动创建数据库结构，生产环境慎用
+    }),
+  }),
+    ],
   controllers: [AppController, CatsController],
   providers: [AppService],
 })
--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,26 @@
-import { Controller,Get  } from '@nestjs/common';
+import { Get, Post, Body, Put, Delete, Param, Controller, UsePipes } from '@nestjs/common';
+import { UserService } from './cats.service';
+// import { UserRO } from './user.interface';
+// import { CreateUserDto, UpdateUserDto, LoginUserDto } from './dto';
+// import { HttpException } from '@nestjs/common/exceptions/http.exception';
+// import { User } from './user.decorator';
+// import { ValidationPipe } from '../shared/pipes/validation.pipe';
 
+// import {
+//   ApiBearerAuth, ApiTags
+// } from '@nestjs/swagger';
+
+// @ApiBearerAuth()
+// @ApiTags('user')
 @Controller('cats')
 export class CatsController {
+  constructor(private readonly userService: UserService) {}
     @Get()
     findAll(): string {
       return 'This action returns all cats';
     }
+    @Post()
+      async findOne(@Body('name') name: string): Promise<any> {
+        return await this.userService.findByEmail(name);
+      }
 }
